48afc9cb3e8ded0aacc735e3dba5127e3638060d
nickel-dime <goepnik@gmail.com>
2021-03-25 19:27:52 -0400

    finished implementaitno with tests

--- src/SqueezingPictures.java
+++ src/SqueezingPictures.java
@@ -1,8 +1,9 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+
 import tester.*;
-import javalib.impworld.*;
 import javalib.worldimages.*;
+
 import java.awt.Color;
 import java.util.Deque;
 
@@ -11,7 +12,6 @@ public class SqueezingPictures {
 
   String filename;
   FromFileImage image;
-  // using our deque implementation (zipped)
   Deque<Deque<Pixel>> grid;
 
   public SqueezingPictures(String filename) {
@@ -26,47 +26,66 @@ public class SqueezingPictures {
       Deque<Pixel> curRow = new ArrayDeque<Pixel>();
       ArrayList<Pixel> prevRow = new ArrayList<Pixel>();
 
+      // if there already is a row, we can set the previous row to that value
       if (!grid.isEmpty()) {
         prevRow = new ArrayList<Pixel>(grid.peekLast());
       }
 
+      // for each pixel in the row, insert it into the deque
       for (int j = 0; j < image.getWidth(); j += 1) {
         Pixel curPixel;
         if (i == 0 && j == 0) {
           // top left corner
-          curPixel = new Pixel(this.image.getColorAt(j, i), new EdgePixel(), null, null, new EdgePixel());
+          curPixel = new Pixel(this.image.getColorAt(j, i), new UnknownPixel(),
+                  new UnknownPixel(), new UnknownPixel(), new UnknownPixel());
 
-        } else if (i == 0 && j == image.getWidth()) {
+        }
+        else if (i == 0 && j == image.getWidth()) {
           // top right corner
-          curPixel = new Pixel(this.image.getColorAt(j, i), new EdgePixel(), new EdgePixel(), null, null);
+          curPixel = new Pixel(this.image.getColorAt(j, i), new UnknownPixel(),
+                  new UnknownPixel(), new UnknownPixel(), curRow.peekLast());
 
-        } else if (i == image.getHeight() && j == 0) {
+        }
+        else if (i == image.getHeight() && j == 0) {
           // bottom left corner
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new EdgePixel(), null, new EdgePixel());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), new UnknownPixel());
 
-        } else if (i == image.getHeight() && j == image.getWidth()) {
+        }
+        else if (i == image.getHeight() && j == image.getWidth()) {
           // bottom right corner
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new EdgePixel(), new EdgePixel(), curRow.peekLast());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), curRow.peekLast());
 
-        } else if (i == 0) {
+        }
+        else if (i == 0) {
           // top row
-          curPixel = new Pixel(this.image.getColorAt(j, i), new EdgePixel(), null, null, curRow.peekLast());
+          curPixel = new Pixel(this.image.getColorAt(j, i), new UnknownPixel(),
+                  new UnknownPixel(), new UnknownPixel(), curRow.peekLast());
 
-        } else if (i == image.getHeight()) {
+        }
+        else if (i == image.getHeight()) {
           // bottom row
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), null, new EdgePixel(), curRow.peekLast());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), curRow.peekLast());
 
-        } else if (j == 0) {
+        }
+        else if (j == 0) {
           // left column
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), null, null, new EdgePixel());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), new UnknownPixel());
 
-        } else if (j == image.getWidth()) {
+        }
+        else if (j == image.getWidth()) {
           // right column
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new EdgePixel(), null, curRow.peekLast());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), curRow.peekLast());
 
-        } else {
+        }
+        else {
           // no edges
-          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), null, null, curRow.peekLast());
+          curPixel = new Pixel(this.image.getColorAt(j, i), prevRow.get(j), new UnknownPixel(),
+                  new UnknownPixel(), curRow.peekLast());
 
         }
         curRow.addLast(curPixel);
@@ -76,6 +95,7 @@ public class SqueezingPictures {
   }
 }
 
+// represents a pixel
 interface IPixel {
 
   // gets the brightness of a pixel
@@ -87,54 +107,66 @@ interface IPixel {
   // get pixels in 4 different directions
   // gets the pixel above
   IPixel getUp();
+
   // gets the pixel to the right
   IPixel getRight();
+
   // gets the pixel below
   IPixel getDown();
+
   // gets the pixel to the left
   IPixel getLeft();
 
   // sets pixels in 4 different directions
   void setUp(IPixel newUp);
+
+  // EFFECT: sets this pixel's right to another pixel
   void setRight(IPixel right);
+
+  // EFFECT: sets this pixel's down to another pixel
   void setDown(IPixel down);
+
+  // EFFECT: sets this pixel's left to another pixel
   void setLeft(IPixel left);
 
+  // checks to see if this pixel is valid
+  boolean validPixel();
+
 }
+
+class SeamInfo {
+
+  Pixel currentPixel;
+  double totalWeight;
+  SeamInfo cameFrom;
+
+  public SeamInfo(Pixel currentPixel, double totalWeight, SeamInfo cameFrom) {
+    
+  }
+
+}
+
 
 // constructs an individual pixel with references to neighbors
 class Pixel implements IPixel {
 
   Color color;
 
-  // neighbors
-  // We decided to use 8 individual fields because the number of fields never changes
-  // and it is easier to access and adjust them without using indices or a complicated data structure
+  // the 4 neighbors
+  // in order to access topright, we would use up then right
   IPixel up;
   IPixel right;
   IPixel down;
   IPixel left;
 
-  // energy fields
-  double energy;
-
   // creates a pixel with pixels in other directions
-
+  // EFFECT: modifies up, right, down, and lefts' references to include this new pixel
   public Pixel(Color color, IPixel up, IPixel right, IPixel down, IPixel left) {
+    this.color = color;
     this.up = up;
     this.right = right;
     this.down = down;
     this.left = left;
-
-    // two pixels are only the same if they are the same object reference
-    // .equals here is used for referential equality, which is what we want to check
-    // since ** TO DO **
-//    if (!(this.getLeft().getUp().equals(this.getUp().getLeft())
-//            && this.getUp().getRight().equals(this.getRight().getUp())
-//            && this.getRight().getDown().equals(this.getDown().getRight())
-//            && this.getDown().getLeft().equals(this.getLeft().getDown()))) {
-//      throw new IllegalArgumentException("This pixel is not well formed");
-//    }
 
     this.getUp().setDown(this);
     this.getRight().setLeft(this);
@@ -156,9 +188,11 @@ class Pixel implements IPixel {
 
   // gets the eneryg of a pixel
   public double getEnergy() {
-    double horizontalEnergy = (this.getUp().getLeft().getBrightness() + (2 * this.getLeft().getBrightness()) + this.getLeft().getBrightness())
+    double horizontalEnergy =
+            (this.getUp().getLeft().getBrightness() + (2 * this.getLeft().getBrightness()) + this.getLeft().getBrightness())
             - (this.getUp().getRight().getBrightness() + (2 * (this.getRight().getBrightness())) + this.getDown().getRight().getBrightness());
-    double verticalEnergy = (this.getUp().getLeft().getBrightness() + (2 * this.getUp().getBrightness()) + this.getUp().getRight().getBrightness())
+    double verticalEnergy =
+            (this.getUp().getLeft().getBrightness() + (2 * this.getUp().getBrightness()) + this.getUp().getRight().getBrightness())
             - (this.getDown().getLeft().getBrightness() + (2 * (this.getDown().getBrightness())) + this.getDown().getRight().getBrightness());
     return Math.sqrt(Math.pow(horizontalEnergy, 2) + Math.pow(verticalEnergy, 2));
   }
@@ -200,9 +234,25 @@ class Pixel implements IPixel {
     this.left = left;
   }
 
+  // is this pixel valid?
+  // two pixels are only the same if they are the same object reference
+  // .equals here is used for referential equality, which is what we want to check
+  // since a pixel doesn't internally contain its location
+  public boolean validPixel() {
+    return this.getLeft().getUp().equals(this.getUp().getLeft())
+            && this.getUp().getRight().equals(this.getRight().getUp())
+            && this.getRight().getDown().equals(this.getDown().getRight())
+            && this.getDown().getLeft().equals(this.getLeft().getDown());
+  }
+
+  // equals method - because we can't use referential equality
+  // getBrightness(), getEnergy(), Color
+
 }
 
-class EdgePixel implements IPixel {
+// this is a pixel that we don't know because it's either not in the image
+// or has not been processed yet
+class UnknownPixel implements IPixel {
 
   public double getBrightness() {
     return 0;
@@ -213,19 +263,19 @@ class EdgePixel implements IPixel {
   }
 
   public IPixel getUp() {
-    return new EdgePixel();
+    return new UnknownPixel();
   }
 
   public IPixel getRight() {
-    return new EdgePixel();
+    return new UnknownPixel();
   }
 
   public IPixel getDown() {
-    return new EdgePixel();
+    return new UnknownPixel();
   }
 
   public IPixel getLeft() {
-    return new EdgePixel();
+    return new UnknownPixel();
   }
 
   public void setUp(IPixel newUp) {
@@ -236,26 +286,100 @@ class EdgePixel implements IPixel {
 
   }
 
+  // sets the pixel below
   public void setDown(IPixel down) {
 
   }
 
+  // sets the left pixel
   public void setLeft(IPixel left) {
 
+  }
+
+  // is this pixel valid?
+  public boolean validPixel() {
+    return true;
   }
 }
 
 
 class ExamplesSqueezingPictures {
-  SqueezingPictures sp;
+  SqueezingPictures eightBy6;
+  SqueezingPictures balloons;
+  UnknownPixel ukPixel;
+  IPixel redLine1;
+  IPixel redLine2;
+  IPixel redLine3;
+  IPixel redLine4;
+
+  IPixel greenLineNEU;
+  IPixel greenLineSymphony;
+
+  // deque of deque of pixels (to check constructor)
 
   void initTestConditions() {
-    this.sp = new SqueezingPictures("balloons.jpg");
-    System.out.println(sp.image.getWidth() + " " + sp.image.getHeight());
+    this.eightBy6 = new SqueezingPictures("8x6.jpeg");
+    this.balloons = new SqueezingPictures("balloons.jpg");
 
+    this.ukPixel = new UnknownPixel();
+    this.redLine1 =  new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), new UnknownPixel());
+    this.redLine2 = new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), redLine1);
+    this.redLine3 = new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), redLine2);
+    this.redLine4 = new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), redLine3);
+
+    this.greenLineNEU = new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), new UnknownPixel());
+    this.greenLineSymphony = new Pixel(Color.RED, new UnknownPixel(), new UnknownPixel(), new UnknownPixel(), new UnknownPixel());
   }
 
-  void testStuff(Tester t) {
+  // we choose to test all getters at once because they all simply return a field's value
+  void testGetters(Tester t) {
     this.initTestConditions();
   }
+
+  // testing all the setters
+  // we are testing all the setters because they only modify a field
+  void testSetters(Tester t) {
+    this.initTestConditions();
+
+    this.greenLineNEU.setUp(this.greenLineSymphony);
+    t.checkExpect(this.greenLineNEU.getUp(), this.greenLineSymphony);
+
+    this.initTestConditions();
+
+    this.greenLineNEU.setRight(this.greenLineSymphony);
+    t.checkExpect(this.greenLineNEU.getRight(), this.greenLineSymphony);
+
+    this.initTestConditions();
+
+    this.greenLineNEU.setDown(this.greenLineSymphony);
+    t.checkExpect(this.greenLineNEU.getDown(), this.greenLineSymphony);
+
+    this.initTestConditions();
+
+    this.greenLineNEU.setLeft(this.greenLineSymphony);
+    t.checkExpect(this.greenLineNEU.getLeft(), this.greenLineSymphony);
+
+  }
+
+  // tests the grid
+  void testGrid(Tester t) {
+    this.initTestConditions();
+
+    t.checkExpect(this.eightBy6.grid.peekLast(), null);
+  }
+
+  // tests validPixel
+//  void testValidPixel(Tester t) {
+//    this.initTestConditions();
+//
+//    t.checkExpect(this.redLine1.validPixel(), true);
+//    t.checkExpect(this.redLine2.validPixel(), true);
+//
+//    this.redLine2.setLeft(this.greenLineNEU);
+//
+//    t.checkExpect(this.redLine2.validPixel(), false);
+//
+//
+//  }
+
 }
